1c0a4e58583d5cf71eeea7d97cd35a7584963a79
Armand Halbert <ahalbert@cs.umass.edu>
2013-11-24 19:33:30 -0500

    Added State generator for the partitions.

--- fsa.js
+++ fsa.js
@@ -1,5 +1,7 @@
 var graph = new joint.dia.Graph;
 
+var data = { "log": [ { "traceID": 0, "events": [ { "eventIndex": 0, "eventType": "c", "timestamp": 0 }, { "eventIndex": 1, "eventType": "e", "timestamp": 3 }, { "eventIndex": 2, "eventType": "f", "timestamp": 4 } ] }, { "traceID": 1, "events": [ { "eventIndex": 0, "eventType": "a", "timestamp": 10 }, { "eventIndex": 1, "eventType": "c", "timestamp": 13 }, { "eventIndex": 2, "eventType": "e", "timestamp": 14 }, { "eventIndex": 3, "eventType": "f", "timestamp": 17 } ] }, { "traceID": 2, "events": [ { "eventIndex": 0, "eventType": "b", "timestamp": 20 }, { "eventIndex": 1, "eventType": "d", "timestamp": 21 }, { "eventIndex": 2, "eventType": "e", "timestamp": 25 }, { "eventIndex": 3, "eventType": "g", "timestamp": 29 } ] }, { "traceID": 3, "events": [ { "eventIndex": 0, "eventType": "d", "timestamp": 30 }, { "eventIndex": 1, "eventType": "e", "timestamp": 34 }, { "eventIndex": 2, "eventType": "g", "timestamp": 35 } ] }, { "traceID": 4, "events": [ { "eventIndex": 0, "eventType": "d", "timestamp": 40 }, { "eventIndex": 1, "eventType": "e", "timestamp": 41 }, { "eventIndex": 2, "eventType": "g", "timestamp": 45 } ] }, { "traceID": 5, "events": [ { "eventIndex": 0, "eventType": "d", "timestamp": 50 } ] } ], "partitions": [ { "eventType": "c", "events": [ { "traceID": 0, "eventIndex": 0 } ] }, { "eventType": "e", "events": [ { "traceID": 2, "eventIndex": 2 }, { "traceID": 4, "eventIndex": 1 } ] }, { "eventType": "f", "events": [ { "traceID": 0, "eventIndex": 2 }, { "traceID": 1, "eventIndex": 3 } ] }, { "eventType": "a", "events": [ { "traceID": 1, "eventIndex": 0 } ] }, { "eventType": "b", "events": [ { "traceID": 2, "eventIndex": 0 } ] }, { "eventType": "d", "events": [ { "traceID": 2, "eventIndex": 1 }, { "traceID": 5, "eventIndex": 0 } ] }, { "eventType": "g", "events": [ { "traceID": 2, "eventIndex": 3 }, { "traceID": 3, "eventIndex": 2 }, { "traceID": 4, "eventIndex": 2 } ] }, { "eventType": "c", "events": [ { "traceID": 1, "eventIndex": 1 } ] }, { "eventType": "e", "events": [ { "traceID": 0, "eventIndex": 1 } ] }, { "eventType": "d", "events": [ { "traceID": 3, "eventIndex": 0 }, { "traceID": 4, "eventIndex": 0 } ] }, { "eventType": "e", "events": [ { "traceID": 3, "eventIzndex": 1 } ] }, { "eventType": "e", "events": [ { "traceID": 1, "eventIndex": 2 } ] } ], "invariants": [ { "invariantType": "AlwaysFollowedBy", "predicates": [ "a", "c" ], "constraints": [ "lowerbound=3" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "a", "c" ], "constraints": [ "upperbound=3" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "a", "e" ], "constraints": [ "lowerbound=4" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "a", "e" ], "constraints": [ "upperbound=4" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "a", "f" ], "constraints": [ "lowerbound=7" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "a", "f" ], "constraints": [ "upperbound=7" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "c", "e" ], "constraints": [ "lowerbound=1" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "c", "e" ], "constraints": [ "upperbound=3" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "c", "f" ], "constraints": [ "lowerbound=4" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "c", "f" ], "constraints": [ "upperbound=4" ] }, { "invariantType": "AlwaysPrecedes", "predicates": [ "c", "f" ], "constraints": [ "lowerbound=4" ] }, { "invariantType": "AlwaysPrecedes", "predicates": [ "c", "f" ], "constraints": [ "upperbound=4" ] }, { "invariantType": "AlwaysPrecedes", "predicates": [ "e", "f" ], "constraints": [ "lowerbound=1" ] }, { "invariantType": "AlwaysPrecedes", "predicates": [ "e", "f" ], "constraints": [ "upperbound=3" ] }, { "invariantType": "AlwaysPrecedes", "predicates": [ "e", "g" ], "constraints": [ "lowerbound=1" ] }, { "invariantType": "AlwaysPrecedes", "predicates": [ "e", "g" ], "constraints": [ "upperbound=4" ] }, { "invariantType": "AlwaysPrecedes", "predicates": [ "d", "g" ], "constraints": [ "lowerbound=5" ] }, { "invariantType": "AlwaysPrecedes", "predicates": [ "d", "g" ], "constraints": [ "upperbound=8" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "b", "e" ], "constraints": [ "lowerbound=5" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "b", "e" ], "constraints": [ "upperbound=5" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "b", "d" ], "constraints": [ "lowerbound=1" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "b", "d" ], "constraints": [ "upperbound=1" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "b", "g" ], "constraints": [ "lowerbound=9" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "b", "g" ], "constraints": [ "upperbound=9" ] } ] }; 
+
 var paper = new joint.dia.Paper({
     el: $('#paper'),
     width: 800,
@@ -8,13 +10,17 @@ var paper = new joint.dia.Paper({
     model: graph
 });
 
-function state(x, y, label) {
-    
+function state(x, y, eventdata) {
+        
     var cell = new joint.shapes.fsa.State({
         position: { x: x, y: y },
-        size: { width: 60, height: 60 },
-        attrs: { text : { text: label }}
+        size: { width: 10, height: 10 },
+        attrs: { text : { text: eventdata.eventType }}
     });
+    cell.events = [];
+    for (var i = 0; i < eventdata.events.length; i ++) {
+        cell.events.push(eventdata.events[i]);
+    }
     graph.addCell(cell);
     return cell;
 };
@@ -31,24 +37,58 @@ function link(source, target, label, vertices) {
     return cell;
 }
 
+
+
+function generateStates() {
+    var init  = state(10, 10, { "eventType": "init", "events": [] });
+    var term = state(340, 340, {"eventType": "term", "events": [] });
+    var states = [];
+    var startx = 10;
+    var starty = 70;
+    for (var i = 0; i < data.partitions.length; i++  ){
+            states.push(state(startx,starty,data.partitions[i]));
+            startx += 100;
+            if (startx > 550 ){
+                starty += 75;
+                startx = 10;
+            }
+        }
+}
+
+function generateTransitions() {
+
+}
+
+function getRandomInt (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function findState(eventType, traceid, index) {
+    for (var i = 0; i < states.length; i++) {
+    }
+    return null;
+}
+
+// function findState(eventData) {
+//     for (var i = 0; i < states.length; i++) {
+//         if(isMatchingState(states[i], eventData))
+//     }
+//     return null;
+// }
+
+function isMatchingState(state, eventData) {
+    for (var i = 0; i < state.events.length; i++) {
+         if (state.events[i].eventType == eventData.eventType) {
+            if (state.events[i].traceID == eventData.traceID) {
+                if (state.events[i].eventIndex == eventData.eventIndex) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
 var start = new joint.shapes.fsa.StartState({ position: { x: 50, y: 530 } });
 graph.addCell(start);
-
-var code  = state(180, 390, 'code');
-var slash = state(340, 220, 'slash');
-var star  = state(600, 400, 'star');
-var line  = state(190, 100, 'line');
-var block = state(560, 140, 'block');
-
-link(start, code,  'start');
-link(code,  slash, '/');
-link(slash, code,  'other', [{x: 270, y: 300}]);
-link(slash, line,  '/');
-link(line,  code,  'new\n line');
-link(slash, block, '*');
-link(block, star,  '*');
-link(star,  block, 'other', [{x: 650, y: 290}]);
-link(star,  code,  '/',     [{x: 490, y: 310}]);
-link(line,  line,  'other', [{x: 115,y: 100}, {x: 250, y: 50}]);
-link(block, block, 'other', [{x: 485,y: 140}, {x: 620, y: 90}]);
-link(code,  code,  'other', [{x: 180,y: 500}, {x: 305, y: 450}]);
+generateStates();
