0589bc5ab56d049430564eb0205b698c06f40a74
Ariunjargal Batjargal <abatjarg@umass.edu>
2013-11-28 22:05:37 -0500

    Made change to the invariant.js and invariant tab. invariant.js not working with the tabbed interface, using invariant.html

--- invariant.js
+++ invariant.js
@@ -1,261 +1,5 @@
 //For Debuging purpose and local usage
 var invariant = {
-    "log": [
-        {
-            "traceID": 0,
-            "events": [
-                {
-                    "eventIndex": 0,
-                    "eventType": "c",
-                    "timestamp": 0
-                },
-                {
-                    "eventIndex": 1,
-                    "eventType": "e",
-                    "timestamp": 3
-                },
-                {
-                    "eventIndex": 2,
-                    "eventType": "f",
-                    "timestamp": 4
-                }
-            ]
-        },
-        {
-            "traceID": 1,
-            "events": [
-                {
-                    "eventIndex": 0,
-                    "eventType": "a",
-                    "timestamp": 10
-                },
-                {
-                    "eventIndex": 1,
-                    "eventType": "c",
-                    "timestamp": 13
-                },
-                {
-                    "eventIndex": 2,
-                    "eventType": "e",
-                    "timestamp": 14
-                },
-                {
-                    "eventIndex": 3,
-                    "eventType": "f",
-                    "timestamp": 17
-                }
-            ]
-        },
-        {
-            "traceID": 2,
-            "events": [
-                {
-                    "eventIndex": 0,
-                    "eventType": "b",
-                    "timestamp": 20
-                },
-                {
-                    "eventIndex": 1,
-                    "eventType": "d",
-                    "timestamp": 21
-                },
-                {
-                    "eventIndex": 2,
-                    "eventType": "e",
-                    "timestamp": 25
-                },
-                {
-                    "eventIndex": 3,
-                    "eventType": "g",
-                    "timestamp": 29
-                }
-            ]
-        },
-        {
-            "traceID": 3,
-            "events": [
-                {
-                    "eventIndex": 0,
-                    "eventType": "d",
-                    "timestamp": 30
-                },
-                {
-                    "eventIndex": 1,
-                    "eventType": "e",
-                    "timestamp": 34
-                },
-                {
-                    "eventIndex": 2,
-                    "eventType": "g",
-                    "timestamp": 35
-                }
-            ]
-        },
-        {
-            "traceID": 4,
-            "events": [
-                {
-                    "eventIndex": 0,
-                    "eventType": "d",
-                    "timestamp": 40
-                },
-                {
-                    "eventIndex": 1,
-                    "eventType": "e",
-                    "timestamp": 41
-                },
-                {
-                    "eventIndex": 2,
-                    "eventType": "g",
-                    "timestamp": 45
-                }
-            ]
-        },
-        {
-            "traceID": 5,
-            "events": [
-                {
-                    "eventIndex": 0,
-                    "eventType": "d",
-                    "timestamp": 50
-                }
-            ]
-        }
-    ],
-    "partitions": [
-        {
-            "eventType": "c",
-            "events": [
-                {
-                    "traceID": 0,
-                    "eventIndex": 0
-                }
-            ]
-        },
-        {
-            "eventType": "e",
-            "events": [
-                {
-                    "traceID": 2,
-                    "eventIndex": 2
-                },
-                {
-                    "traceID": 4,
-                    "eventIndex": 1
-                }
-            ]
-        },
-        {
-            "eventType": "f",
-            "events": [
-                {
-                    "traceID": 0,
-                    "eventIndex": 2
-                },
-                {
-                    "traceID": 1,
-                    "eventIndex": 3
-                }
-            ]
-        },
-        {
-            "eventType": "a",
-            "events": [
-                {
-                    "traceID": 1,
-                    "eventIndex": 0
-                }
-            ]
-        },
-        {
-            "eventType": "b",
-            "events": [
-                {
-                    "traceID": 2,
-                    "eventIndex": 0
-                }
-            ]
-        },
-        {
-            "eventType": "d",
-            "events": [
-                {
-                    "traceID": 2,
-                    "eventIndex": 1
-                },
-                {
-                    "traceID": 5,
-                    "eventIndex": 0
-                }
-            ]
-        },
-        {
-            "eventType": "g",
-            "events": [
-                {
-                    "traceID": 2,
-                    "eventIndex": 3
-                },
-                {
-                    "traceID": 3,
-                    "eventIndex": 2
-                },
-                {
-                    "traceID": 4,
-                    "eventIndex": 2
-                }
-            ]
-        },
-        {
-            "eventType": "c",
-            "events": [
-                {
-                    "traceID": 1,
-                    "eventIndex": 1
-                }
-            ]
-        },
-        {
-            "eventType": "e",
-            "events": [
-                {
-                    "traceID": 0,
-                    "eventIndex": 1
-                }
-            ]
-        },
-        {
-            "eventType": "d",
-            "events": [
-                {
-                    "traceID": 3,
-                    "eventIndex": 0
-                },
-                {
-                    "traceID": 4,
-                    "eventIndex": 0
-                }
-            ]
-        },
-        {
-            "eventType": "e",
-            "events": [
-                {
-                    "traceID": 3,
-                    "eventIzndex": 1
-                }
-            ]
-        },
-        {
-            "eventType": "e",
-            "events": [
-                {
-                    "traceID": 1,
-                    "eventIndex": 2
-                }
-            ]
-        }
-    ],
     "invariants": [
         {
             "invariantType": "AlwaysFollowedBy",
@@ -500,26 +244,132 @@ var invariant = {
     ]
 };
 
-function getInvaraints(data) {
-var alwaysPrecedes = "";
-var alwaysFollowedBy = "";
-var constraintText="";
-for (var i = 0; i < invariant.invariants.length; i++) {
-    if(invariant.invariants[i].invariantType === "AlwaysPrecedes"){
-      alwaysPrecedes = invariant.invariants[i].predicates[0] + " <--- " + invariant.invariants[i].predicates[1] + " " + constraintText;
-      constraintText = "";
-      for (var j = 0; j < invariant.invariants[i].constraints.length; j++) {
-          constraintText += invariant.invariants[i].constraints[j] + "\n"; //Add information about bounds
-      }
-      $("#alwaysPrecedes").append("<tr><td>" + alwaysPrecedes + "</td></tr>");
-    }else if(invariant.invariants[i].invariantType === "AlwaysFollowedBy"){
-      constraintText = "";
-      for (var j = 0; j < invariant.invariants[i].constraints.length; j++) {
-          constraintText += invariant.invariants[i].constraints[j] + "\n"; //Add information about bounds
-      }
-      alwaysFollowedBy = invariant.invariants[i].predicates[0] + " ---> " + invariant.invariants[i].predicates[1] + " " + constraintText;
-      $("#alwaysFollowedBy").append("<tr><td>" + alwaysFollowedBy + "</td></tr>");
-	}
-	}
+
+//Organizing the data into managable arrays
+
+var alwaysPrecedes = [];
+var alwaysFollowedBy = [];
+
+function checkExistsPrecedes(data){
+    var result = false;
+    for(var i=0; i<alwaysPrecedes.length;i++){
+        if(alwaysPrecedes[i].predicates[0] === data.predicates[0] && alwaysPrecedes[i].predicates[1] === data.predicates[1]){
+            alwaysPrecedes[i].constraints.push(data.constraints[0]);
+            result = true;
+            break;
+        }
+    }
+    return result;
 }
-getInvaraints(null);
+
+function checkExistsFollowed(data){
+    var result = false;
+    for(var i=0; i<alwaysFollowedBy.length;i++){
+        if(alwaysFollowedBy[i].predicates[0] === data.predicates[0] && alwaysFollowedBy[i].predicates[1] === data.predicates[1]){
+            alwaysFollowedBy[i].constraints.push(data.constraints[0]);
+            result = true;
+            break;
+        }
+    }
+    return result;
+}
+
+function getPredicates(data){
+    for(var i=0; i<invariant.invariants.length; i++){
+        if(invariant.invariants[i].invariantType === "AlwaysPrecedes"){
+            if(checkExistsPrecedes(invariant.invariants[i]) === false){alwaysPrecedes.push(invariant.invariants[i]);}
+        }
+        if(invariant.invariants[i].invariantType === "AlwaysFollowedBy"){
+            if(checkExistsFollowed(invariant.invariants[i]) === false){alwaysFollowedBy.push(invariant.invariants[i]);}
+        }
+    }
+}
+
+//Printing and Graphing the invariants
+
+var graph = new joint.dia.Graph;
+
+var paper = new joint.dia.Paper({
+    el: $('#myholder'),
+    width: 600,
+    height: 600,
+    model: graph
+});
+
+function drawGraph(){
+    var space = 10;
+    for(var i=0; i<alwaysPrecedes.length; i++){
+        $("#alwaysPrecedes").append("<tr><td>" + alwaysPrecedes[i].predicates[0] + "<--" + 
+            alwaysPrecedes[i].predicates[1] + "</td></tr>")
+        var box1 = new joint.shapes.basic.Rect({
+        position: { x: 20, y: 10 +space},
+        size: { width: 100, height: 30 },
+        attrs: { rect: { fill: 'blue' }, text: { text: alwaysPrecedes[i].predicates[0], fill: 'white' } }
+        });
+        var box2 = new joint.shapes.basic.Rect({
+        position: { x: 420, y: 10 +space},
+        size: { width: 100, height: 30 },
+        attrs: { rect: { fill: 'blue' }, text: { text: alwaysPrecedes[i].predicates[1], fill: 'white' } }
+        });
+        var link = new joint.dia.Link({
+        source: { id: box1.id },
+        target: { id: box2.id }
+        }); 
+        link.attr({
+        '.connection': { stroke: 'blue' },
+        '.marker-source': { fill: 'red', d: 'M 10 0 L 0 5 L 10 10 z' },
+        }); 
+        graph.addCells([box1,box2,link]);
+        space +=50;
+    }
+    for(var i=0; i<alwaysFollowedBy.length; i++){
+        $("#alwaysFollowedBy").append("<tr><td>" + alwaysFollowedBy[i].predicates[0] + "-->" + 
+            alwaysFollowedBy[i].predicates[1] + "</td></tr>")
+        var box1 = new joint.shapes.basic.Rect({
+        position: { x: 20, y: 10 +space},
+        size: { width: 100, height: 30 },
+        attrs: { rect: { fill: 'blue' }, text: { text: alwaysFollowedBy[i].predicates[0], fill: 'white' } }
+        });
+        var box2 = new joint.shapes.basic.Rect({
+        position: { x: 420, y: 10 +space},
+        size: { width: 100, height: 30 },
+        attrs: { rect: { fill: 'blue' }, text: { text: alwaysFollowedBy[i].predicates[1], fill: 'white' } }
+        });
+        var link = new joint.dia.Link({
+        source: { id: box2.id },
+        target: { id: box1.id }
+        }); 
+        link.attr({
+        '.connection': { stroke: 'blue' },
+        '.marker-source': { fill: 'red', d: 'M 10 0 L 0 5 L 10 10 z' },
+        }); 
+        graph.addCells([box1,box2,link]);
+        space +=50;
+    }
+}
+
+
+// function getInvaraints(data) {
+// var alwaysPrecedes = "";
+// var alwaysFollowedBy = "";
+// var constraintText="";
+// for (var i = 0; i < invariant.invariants.length; i++) {
+//     if(invariant.invariants[i].invariantType === "AlwaysPrecedes"){
+//       alwaysPrecedes = invariant.invariants[i].predicates[0] + " <--- " + invariant.invariants[i].predicates[1] + " " + constraintText;
+//       constraintText = "";
+//       for (var j = 0; j < invariant.invariants[i].constraints.length; j++) {
+//           constraintText += invariant.invariants[i].constraints[j] + "\n"; //Add information about bounds
+//       }
+//       $("#alwaysPrecedes").append("<tr><td>" + alwaysPrecedes + "</td></tr>");
+//     }else if(invariant.invariants[i].invariantType === "AlwaysFollowedBy"){
+//       constraintText = "";
+//       for (var j = 0; j < invariant.invariants[i].constraints.length; j++) {
+//           constraintText += invariant.invariants[i].constraints[j] + "\n"; //Add information about bounds
+//       }
+//       alwaysFollowedBy = invariant.invariants[i].predicates[0] + " ---> " + invariant.invariants[i].predicates[1] + " " + constraintText;
+//       $("#alwaysFollowedBy").append("<tr><td>" + alwaysFollowedBy + "</td></tr>");
+// 	}
+// 	}
+// }
+getPredicates(null);
+drawGraph();
