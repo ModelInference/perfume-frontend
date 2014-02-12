341de79bd3888b969b9c6bc28d4bf45b840dc583
Armand Halbert <ahalbert@cs.umass.edu>
2014-02-12 17:05:45 -0500

    Code for making an AJAX call for server, merged model and form pages.

--- fsa.js
+++ fsa.js
@@ -2,6 +2,10 @@ var graph = new joint.dia.Graph;
 //Test data
 
 var data = { "log": [ { "traceID": 0, "events": [ { "eventIndex": 0, "eventType": "c", "timestamp": 0 }, { "eventIndex": 1, "eventType": "e", "timestamp": 3 }, { "eventIndex": 2, "eventType": "f", "timestamp": 4 } ] }, { "traceID": 1, "events": [ { "eventIndex": 0, "eventType": "a", "timestamp": 10 }, { "eventIndex": 1, "eventType": "c", "timestamp": 13 }, { "eventIndex": 2, "eventType": "e", "timestamp": 14 }, { "eventIndex": 3, "eventType": "f", "timestamp": 17 } ] }, { "traceID": 2, "events": [ { "eventIndex": 0, "eventType": "b", "timestamp": 20 }, { "eventIndex": 1, "eventType": "d", "timestamp": 21 }, { "eventIndex": 2, "eventType": "e", "timestamp": 25 }, { "eventIndex": 3, "eventType": "g", "timestamp": 29 } ] }, { "traceID": 3, "events": [ { "eventIndex": 0, "eventType": "d", "timestamp": 30 }, { "eventIndex": 1, "eventType": "e", "timestamp": 34 }, { "eventIndex": 2, "eventType": "g", "timestamp": 35 } ] }, { "traceID": 4, "events": [ { "eventIndex": 0, "eventType": "d", "timestamp": 40 }, { "eventIndex": 1, "eventType": "e", "timestamp": 41 }, { "eventIndex": 2, "eventType": "g", "timestamp": 45 } ] }, { "traceID": 5, "events": [ { "eventIndex": 0, "eventType": "d", "timestamp": 50 } ] } ], "partitions": [ { "eventType": "c", "events": [ { "traceID": 0, "eventIndex": 0 } ] }, { "eventType": "e", "events": [ { "traceID": 2, "eventIndex": 2 }, { "traceID": 4, "eventIndex": 1 } ] }, { "eventType": "f", "events": [ { "traceID": 0, "eventIndex": 2 }, { "traceID": 1, "eventIndex": 3 } ] }, { "eventType": "a", "events": [ { "traceID": 1, "eventIndex": 0 } ] }, { "eventType": "b", "events": [ { "traceID": 2, "eventIndex": 0 } ] }, { "eventType": "d", "events": [ { "traceID": 2, "eventIndex": 1 }, { "traceID": 5, "eventIndex": 0 } ] }, { "eventType": "g", "events": [ { "traceID": 2, "eventIndex": 3 }, { "traceID": 3, "eventIndex": 2 }, { "traceID": 4, "eventIndex": 2 } ] }, { "eventType": "c", "events": [ { "traceID": 1, "eventIndex": 1 } ] }, { "eventType": "e", "events": [ { "traceID": 0, "eventIndex": 1 } ] }, { "eventType": "d", "events": [ { "traceID": 3, "eventIndex": 0 }, { "traceID": 4, "eventIndex": 0 } ] }, { "eventType": "e", "events": [ { "traceID": 3, "eventIndex": 1 } ] }, { "eventType": "e", "events": [ { "traceID": 1, "eventIndex": 2 } ] } ], "invariants": [ { "invariantType": "AlwaysFollowedBy", "predicates": [ "a", "c" ], "constraints": [ "lowerbound=3" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "a", "c" ], "constraints": [ "upperbound=3" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "a", "e" ], "constraints": [ "lowerbound=4" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "a", "e" ], "constraints": [ "upperbound=4" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "a", "f" ], "constraints": [ "lowerbound=7" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "a", "f" ], "constraints": [ "upperbound=7" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "c", "e" ], "constraints": [ "lowerbound=1" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "c", "e" ], "constraints": [ "upperbound=3" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "c", "f" ], "constraints": [ "lowerbound=4" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "c", "f" ], "constraints": [ "upperbound=4" ] }, { "invariantType": "AlwaysPrecedes", "predicates": [ "c", "f" ], "constraints": [ "lowerbound=4" ] }, { "invariantType": "AlwaysPrecedes", "predicates": [ "c", "f" ], "constraints": [ "upperbound=4" ] }, { "invariantType": "AlwaysPrecedes", "predicates": [ "e", "f" ], "constraints": [ "lowerbound=1" ] }, { "invariantType": "AlwaysPrecedes", "predicates": [ "e", "f" ], "constraints": [ "upperbound=3" ] }, { "invariantType": "AlwaysPrecedes", "predicates": [ "e", "g" ], "constraints": [ "lowerbound=1" ] }, { "invariantType": "AlwaysPrecedes", "predicates": [ "e", "g" ], "constraints": [ "upperbound=4" ] }, { "invariantType": "AlwaysPrecedes", "predicates": [ "d", "g" ], "constraints": [ "lowerbound=5" ] }, { "invariantType": "AlwaysPrecedes", "predicates": [ "d", "g" ], "constraints": [ "upperbound=8" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "b", "e" ], "constraints": [ "lowerbound=5" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "b", "e" ], "constraints": [ "upperbound=5" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "b", "d" ], "constraints": [ "lowerbound=1" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "b", "d" ], "constraints": [ "upperbound=1" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "b", "g" ], "constraints": [ "lowerbound=9" ] }, { "invariantType": "AlwaysFollowedBy", "predicates": [ "b", "g" ], "constraints": [ "upperbound=9" ] } ] }; 
+var init  = state(10, 10, { "eventType": "init", "events": [{ "traceID": 0, "eventIndex": 0 }] });
+var term = state(500, 550, {"eventType": "term", "events": [{ "traceID": 0, "eventIndex": 0 }] });
+var states = generateStates(data);
+var links = generateTransitions(data);
 
 var paper = new joint.dia.Paper({
     el: $('#paper'),
@@ -14,14 +18,16 @@ var paper = new joint.dia.Paper({
 
 //Constructor for transition objects. Borrowed from the FSA demo.
 function link(source, target, label, vertices) {
-    
+    var w = parseInt(label,10);
+    if (label == "0") 
+        label = "";
     var cell = new joint.shapes.fsa.Arrow({
         source: { id: source.id },
         target: { id: target.id },
         labels: [{ position: .5, attrs: { text: { text: label || '', 'font-weight': 'bold' } } }],
         vertices: vertices || []
     });
-    cell.weight = parseInt(label,10);
+    cell.weight = w;
     return cell;
 }
 
@@ -222,40 +228,46 @@ function getLinkbyId(id) {
     return null;
 }
 
-var init  = state(10, 10, { "eventType": "init", "events": [{ "traceID": 0, "eventIndex": 0 }] });
-var term = state(500, 550, {"eventType": "term", "events": [{ "traceID": 0, "eventIndex": 0 }] });
-var states = generateStates(data);
-var links = generateTransitions(data);
-var pathLengths = searchForShortestAndLongestPath(term.id);
-var maxPath = pathLengths[3];
-var minPath = pathLengths[2];
+function drawModel(data) {
 
-var l = getLinkByPathId(init.id, pathLengths[3][0],links);
-l.attr({'.connection': { stroke: 'red' }});
-for (var i = 1; i < pathLengths[3].length; i++) {
-    l = getLinkByPathId(pathLengths[3][i-1], pathLengths[3][i], links);
+    init  = state(10, 10, { "eventType": "init", "events": [{ "traceID": 0, "eventIndex": 0 }] });
+    term = state(500, 550, {"eventType": "term", "events": [{ "traceID": 0, "eventIndex": 0 }] });
+    states = generateStates(data);
+    links = generateTransitions(data);
+        
+    var pathLengths = searchForShortestAndLongestPath(term.id);
+    var maxPath = pathLengths[3];
+    var minPath = pathLengths[2];
+
+    var l = getLinkByPathId(init.id, pathLengths[3][0],links);
     l.attr({'.connection': { stroke: 'red' }});
-}
-l = getLinkByPathId(pathLengths[3][pathLengths[3].length-1], term.id, links);
-l.attr({'.connection': { stroke: 'red' }});
+    for (var i = 1; i < pathLengths[3].length; i++) {
+        l = getLinkByPathId(pathLengths[3][i-1], pathLengths[3][i], links);
+        l.attr({'.connection': { stroke: 'red' }});
+    }
+    l = getLinkByPathId(pathLengths[3][pathLengths[3].length-1], term.id, links);
+    l.attr({'.connection': { stroke: 'red' }});
 
-l = getLinkByPathId(init.id, pathLengths[2][0], links);
-l.attr({'.connection': { stroke: 'green' }});
-for (i = 1; i < pathLengths[2].length; i++) {
-    l = getLinkByPathId(pathLengths[2][i-1], pathLengths[2][i], links);
+    l = getLinkByPathId(init.id, pathLengths[2][0], links);
     l.attr({'.connection': { stroke: 'green' }});
-}
-l = getLinkByPathId(pathLengths[2][pathLengths[2].length-1], term.id, links);
-l.attr({'.connection': { stroke: 'green' }});
+    for (i = 1; i < pathLengths[2].length; i++) {
+        l = getLinkByPathId(pathLengths[2][i-1], pathLengths[2][i], links);
+        l.attr({'.connection': { stroke: 'green' }});
+    }
+    l = getLinkByPathId(pathLengths[2][pathLengths[2].length-1], term.id, links);
+    l.attr({'.connection': { stroke: 'green' }});
 
-generateGradients(pathLengths[1]);
-$(".marker-vertex-remove").empty(); //Gets rid of ability to delete states.
-var svg = d3.select("svg");
-var grad = svg.append("linearGradient").attr("id", "grad")
-    .attr("x1", "0%").attr("x2", "0%").attr("y1", "100%").attr("y2", "0%");
-    grad.append("stop").attr("offset", "50%").style("stop-color", "blue");
-    grad.append("stop").attr("offset", "80%").style("stop-color", "white");
-draw();
-$(".tool-remove").empty();
-$(".link-tools").remove();
-$(".marker-arrowheads").remove();
+    generateGradients(pathLengths[1]);
+    $(".marker-vertex-remove").empty(); //Gets rid of ability to delete states.
+    var svg = d3.select("svg");
+    var grad = svg.append("linearGradient").attr("id", "grad")
+        .attr("x1", "0%").attr("x2", "0%").attr("y1", "100%").attr("y2", "0%");
+        grad.append("stop").attr("offset", "50%").style("stop-color", "blue");
+        grad.append("stop").attr("offset", "80%").style("stop-color", "white");
+    draw();
+    $(".tool-remove").empty();
+    $(".link-tools").remove();
+    $(".marker-arrowheads").remove();
+}
+
+drawModel(data);
