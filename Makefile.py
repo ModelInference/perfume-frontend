3a81c3f5aa4299ff1bf877ddb7d6a1d9cac9fb7e
Armand Halbert <ahalbert@cs.umass.edu>
2014-02-20 10:20:25 -0500

    Introduced templating system, moved entire application to index.html.

---
+++ Makefile.py
@@ -0,0 +1,11 @@
+
+from Cheetah.Template import Template
+
+def compile_templates():
+    with open('index_template.html', 'r') as templatefile:
+        template = templatefile.read()
+    compiled_template = Template(template).__str__()
+    with open('index.html', 'w') as postfile:
+        postfile.write(compiled_template) 
+
+compile_templates()
