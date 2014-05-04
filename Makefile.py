b6ff870be2c3463d5aff3e806387b85e38eadb87
Armand Halbert <ahalbert@cs.umass.edu>
2014-05-04 04:09:19 -0400

    #48 Commit SHA now updated by Makefile.py

--- Makefile.py
+++ Makefile.py
@@ -1,11 +1,20 @@
 
+import subprocess
 from Cheetah.Template import Template
 
 def compile_templates():
+    parameters = {"commitSha":getRevision()}
     with open('./template/index_template.html', 'r') as templatefile:
         template = templatefile.read()
-    compiled_template = Template(template).__str__()
+    compiled_template = Template(template, searchList=parameters).__str__()
     with open('index.html', 'w') as postfile:
         postfile.write(compiled_template) 
+
+def getRevision():
+    p = subprocess.Popen(['hg', 'identify'], stdout=subprocess.PIPE,  stderr=subprocess.PIPE)
+    sha, err = p.communicate()
+    sha = sha.split()
+    return sha[0][:-1]
+    
 
 compile_templates()
