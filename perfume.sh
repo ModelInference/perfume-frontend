bd0c03e06cfe18a261c9990cdb6ed883740a4ec2
Tony Ohmann <ohmann@cs.umass.edu>
2015-01-29 13:10:05 -0500

    Updated jar and switched from synoptic.jar to perfume.jar

--- perfume.sh
+++ perfume.sh
@@ -3,4 +3,4 @@
 # Runs Perfume from the compiled JAR file, passing all command
 # line argument directly to main().
 
-java -ea -cp ./syn-bin/synoptic.jar synoptic.main.PerfumeMain $*
+java -jar ./syn-bin/perfume.jar $*
