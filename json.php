58b1da975cb9c555109b45b489281c83f3b3fca3
Tony Ohmann <ohmann@cs.umass.edu>
2015-01-29 13:10:36 -0500

    Stop Perfume backend from creating graphviz output, remove graphviz dependency

--- json.php
+++ json.php
@@ -17,7 +17,7 @@ fwrite($jsonfh, "\n");
 fwrite($jsonfh, "-o /tmp/json\n");
 fwrite($jsonfh, "-j\n");
 fclose($jsonfh);
-$output = shell_exec(' ./perfume.sh -c /tmp/jsonargs.txt ' . "/tmp/log.txt 2>&1");
+$output = shell_exec(' ./perfume.sh --noModelOutput -c /tmp/jsonargs.txt ' . "/tmp/log.txt 2>&1");
 $json = file_get_contents('/tmp/json.json');
 
 $outputfh = fopen("/tmp/jsonout.txt", 'w') or die("can't open file");
