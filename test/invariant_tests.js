3ef7487582efc42fa306dfa49915b1ef0f467e43
Ryan Stanley <stanleyrya@gmail.com>
2015-02-18 04:23:24 -0500

    added first working test

---
+++ test/invariant_tests.js
@@ -0,0 +1,33 @@
+//For an unknown reason, only this test works out of all of these
+test('saysHi()', function() {
+    equal($("#AlwaysPrecedes").length, 0, "AlwaysPrecedes exists!");
+});
+
+// test('alwaysPrecedesExists()', function() {
+//     equal($("#AlwaysPrecedes").length, 0, "AlwaysPrecedes exists!");
+// });
+
+// console.log('loaded invariant_tests.js');
+
+// test('Tables exist', 3, function() {
+// 	equal($("#AlwaysPrecedes").length, 0, "AlwaysPrecedes exists!");
+// 	ok($("#AlwaysFollowedBy").length != 0, "AlwaysFollowedBy exists!");
+// 	ok($("#NeverFollowedBy").length != 0, "NeverFollowedBy exists!");
+// });
+
+// test('Arrays exist', 3, function() {
+// 	ok(alwaysPrecedes, "AlwaysPrecedes exists!");
+// 	ok(alwaysFollowedBy, "AlwaysPrecedes exists!");
+// 	ok(neverFollowedBy, "AlwaysPrecedes exists!");
+// });
+
+// test('first [working] test', function() {
+// 	assert.ok( 1 == "1", "Passed!" );
+// 	console.log('called first test');
+// });
+
+// QUnit.test( "hello test", function( assert ) {
+//   assert.ok( 1 == "1", "Passed!" );
+// });
+
+// QUnit.start();
