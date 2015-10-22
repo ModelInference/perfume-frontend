fc716d0427c088d088616df27b05e87fbe2cb318
Ryan Stanley <stanleyrya@gmail.com>
2015-10-22 17:37:22 -0400

    moved out roundString method to index

--- js/src/invariant.js
+++ js/src/invariant.js
@@ -67,32 +67,9 @@ function findBounds(inv) {
     }
 }
 
-// Round a number in string format
-function roundString(numString) {
-    // If is no decimal, we are already rounded correctly
-    if (numString.indexOf('.') === -1) {
-        return numString;
-    }
-
-    var removeAmount = 0;
-    for (var i = numString.length-1 ; i >= 0; i--) {
-        if (numString[i] === '0') {
-            removeAmount++;
-        }
-        else if (numString[i] === '.') {
-            removeAmount++;
-            break;
-        }
-        else {
-            break;
-        }
-    }
-    return numString.substring(0, numString.length - removeAmount);
-}
-
 function roundBounds(invariants) {
     for (var i = 0; i < invariants.length; i++) {
-        invariants[i].upperbound = roundString(invariants[i].upperbound);
+        invariants[i].upperbound = roundString(invariants[i].upperbound); // index.js
         invariants[i].lowerbound = roundString(invariants[i].lowerbound);
     }
 }
