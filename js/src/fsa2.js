aa5d7fcc3864b00cf628a38dd860855bf448c4cc
Tony Ohmann <ohmann@cs.umass.edu>
2015-10-27 13:01:13 -0400

    Made model state-based

--- js/src/fsa2.js
+++ js/src/fsa2.js
@@ -4,7 +4,7 @@ function generateStates(data) {
     for (var i = 0; i < data.partitions.length; i++) {
         var partition = data.partitions[i];
         var events = JSON.stringify(partition.events);
-        states.push({partition: partition, label:partition.eventType, index:i+2, id:events});
+        states.push({partition: partition, label:"", shape:"circle", index:i+2, id:events});
     }
 }
 
@@ -191,8 +191,8 @@ function drawModel(data) {
     var g = new dagreD3.graphlib.Graph({multigraph:true}).setGraph({});
     states = [];
     links = [];
-    init = {partition: {eventType:"init",events:[]}, label:"init", index:0, nodeclass:"node-initterm"};
-    term = {partition: {eventType:"init",events:[]}, label:"term", index:1, nodeclass:"node-initterm"};
+    init = {partition: {eventType:"INITIAL",events:[]}, shape:"circle", label:"", index:0, nodeclass:"node-initterm"};
+    term = {partition: {eventType:"TERMINAL",events:[]}, shape:"ellipse", label:"TERMINAL", index:1, nodeclass:"node-initterm"};
     states.push(init);
     states.push(term);
     generateStates(data);
@@ -202,7 +202,7 @@ function drawModel(data) {
         g.setNode(i.toString(), states[i]);
     }
     for (i = 0; i < links.length; i++) {
-        var newLabel = links[i].label + '/' + i;
+        var newLabel = links[i].source.partition.eventType + " " + links[i].label + '/' + i;
         var labelShadow = "text-shadow: 0 0 0.4em #fff, 0 0 0.4em #fff, 0 0 0.4em #fff, 0 0 0.4em #fff, 0 0 0.4em #fff, 0 0 0.4em #fff, 0 0 0.4em #fff, 0 0 0.4em #fff, 0 0 0.4em #fff, 0 0 0.4em #fff";
         if (pathAnnotations.maxpath.indexOf(i) !== -1 && pathAnnotations.minpath.indexOf(i) !== -1) {
             g.setEdge(links[i].source.index.toString(), links[i].target.index.toString(), {label:newLabel, labelStyle:labelShadow, id:JSON.stringify(links[i].data), style:'stroke:fuchsia', arrowhead: "vee"});
