87e28ac0eefb595ffe2e32856bf99ff233b3c1bd
Tony Ohmann <ohmann@cs.umass.edu>
2016-01-28 20:00:25 -0500

    Updated dagre-d3. Removed now-unnecessary Chrome 48+ workaround.

--- js/lib/dagre-d3/getTransformToElement.js
+++
@@ -1,2 +0,0 @@
-// Chrome 48+ workaround from http://jointjs.com/blog/get-transform-to-element-polyfill.html
-SVGElement.prototype.getTransformToElement=SVGElement.prototype.getTransformToElement||function(toElement){return toElement.getScreenCTM().inverse().multiply(this.getScreenCTM());};
