883e33a397dd4cae0805bf8779c7b907da85cae9
Tony Ohmann <ohmann@cs.umass.edu>
2016-04-04 16:04:06 -0400

    Added local frontend deployment bash script

---
+++ localdeploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Deploy the Perfume frontend locally. To override the deployment location, run:
+# ./localdeploy.sh desired-deploy-location
+
+# Deployment location
+wwwlocation=/var/www/html/perfume/
+if [ "$1" != "" ]; then
+  wwwlocation=$1
+fi
+echo ===== DEPLOYING PERFUME TO $wwwlocation =====
+
+# Build frontend pages
+echo ===== BUILDING FRONTEND PAGES =====
+python Makefile.py
+
+# Copy all files to actual www directory
+echo ===== DEPLOYING =====
+mkdir -p $wwwlocation
+if [ $? != 0 ]; then
+  echo Could not create $wwwlocation. Quitting
+  exit 1
+fi
+cp -R ./* $wwwlocation
+
+# Prune files not needed by a deployment
+echo ===== PRUNING DEV FILES FROM DEPLOYMENT =====
+cd $wwwlocation
+rm -rf *.sh *.bat *.py *.php *.md template/ syn-bin/
